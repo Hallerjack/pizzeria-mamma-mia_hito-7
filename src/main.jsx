f9576f25787179d4d8284ed49f3281055e638b07
Fernando Gonzalez <fgonzalez@promaticmedia.cl>
2026-05-15 12:13:37 -0400

    ajustes main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import CartProvider from './context/CartContext'
+import PizzaProvider from './context/PizzaContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <PizzaProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </PizzaProvider>
     </BrowserRouter>
   </StrictMode>,
 )
